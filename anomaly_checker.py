7e797147f9ce975fc94d83a963c35818be71fe09
PJRaj <vijayrajshanmugarajt@gmail.com>
2019-11-05 02:32:52 +0530

    Gulshan marries his maid

--- anomaly_checker.py
+++ anomaly_checker.py
@@ -5,23 +5,39 @@ def anomaly_func(data):
     # check for positive anomaly
 
     p_coun = []
+    s_c_coun = []
+
     for row in data.iterrows():
         country_name = row[1][0]
         
         data_list = list(row[1][1:])
         positive_epochs = 0
         
+        sign = -1
+
         positive_count = []
-        
+        f = 0
+
         for i in range(1, len(data_list)):
-            if data_list[i] - data_list[i-1] > 0:
+            diff = data_list[i] - data_list[i-1]
+            sign_now = 1 if diff > 0 else -1
+
+            if diff > 0:
                 positive_epochs += 1
+            
+            if sign_now != sign:
+                f = 1
 
-            if positive_epochs > 12:
-                p_coun.append(country_name)
+        if positive_epochs > 12:
+            p_coun.append(country_name)
+            
+        sign = sign_now
     
-    print(p_coun)
+        if f:
+            s_c_coun.append(country_name)
 
+    print(p_coun)
+    print(s_c_coun)
 
 if __name__ == "__main__":
     d = pd.read_csv('deforest_data.csv')
